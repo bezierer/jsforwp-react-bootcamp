336e33ef49bc57694062124360b06f2e79d46b94
Cameron Campbell <cdcampbell26@gmail.com>
2020-03-27 07:28:16 -0400

    💬 Add some comments.

--- week-1/js/app.js
+++ week-1/js/app.js
@@ -1,14 +1,22 @@
 /**
  * Renders the content & markup for the one-page website.
+ *
+ * Note: In an ideal world this would be JSX & have components broken into single files/directories
+ * But I wanted to go 'basic' here to show understanding of core React based on homework/videos.
  */
 
+// Set up our variables.
 const el = React.createElement;
 const pEl = el('p', {}, '2020 JS for WP React Bootcamp | Cameron Campbell');
 const siteTitle = el('h1', {}, 'Hello React');
 const introContent = el('h2', {}, 'React Bootcamp Week 1 Homework');
-const row = el('div', { className: 'row' }, null);
 
-// Create our Header component.
+/*********************************************
+	Create our Header component.
+
+	Question: Did I go overboard on indtenting
+	trying to make this _more_ readable?
+**********************************************/
 function Header() {
 	return el(
 		'header',
